e8ec283f07aae242877f764bcdfdfab71d9c9fd2
Johnkay010 <kayodejohn010@gmail.com>
2026-08-26 18:43:30 +0100

    Remove trailing slash from CORS_ALLOWED_ORIGINS

--- hebron_backend/settings.py
+++ hebron_backend/settings.py
@@ -97,7 +97,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "https://travel-backend-bcty.onrender.com/",
+    "https://travel-backend-bcty.onrender.com",
 ]
 
 # --- DRF ---------------------------------------------------------
