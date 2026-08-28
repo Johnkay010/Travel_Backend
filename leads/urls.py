709541c8df9bbb6089244d3c25a023fdff2b50ec
Johnkay010 <kayodejohn010@gmail.com>
2026-08-28 10:14:59 +0100

    Added URL

--- leads/urls.py
+++ leads/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 
 from .views import LeadCreateView, PaymentInitializeView, PaymentVerifyView,csrf_test
-
+from django.views.decorators.csrf import csrf_exempt
 urlpatterns = [
     path("test/", csrf_test),
-    path("leads/", LeadCreateView.as_view(), name="lead-create"),
+    path(
+    "leads/",
+    csrf_exempt(LeadCreateView.as_view()),
+    name="lead-create",
+),
     path("payments/initialize/", PaymentInitializeView.as_view(), name="payment-initialize"),
     path("payments/verify/", PaymentVerifyView.as_view(), name="payment-verify"),
 ]
