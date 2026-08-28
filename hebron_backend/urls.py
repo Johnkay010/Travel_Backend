f7dcfdbfb52046f565f99be018fde7f727c7ae49
Johnkay010 <kayodejohn010@gmail.com>
2026-08-28 17:31:15 +0100

    Fix production frontend API and CORS

--- hebron_backend/urls.py
+++ hebron_backend/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("", admin.site.urls),
     path("api/", include("leads.urls")),
+    path("", admin.site.urls),
 ]
